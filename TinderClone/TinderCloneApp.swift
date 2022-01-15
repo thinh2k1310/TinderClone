0ee29895b3e391a95ca93e7717f9e4c33f2c28a5
truong thinh <thinh2k1310@gmail.com>
2022-01-15 13:48:45 +0700

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Truong Thinh on 15/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
